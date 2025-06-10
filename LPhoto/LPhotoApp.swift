342262577595ea7fff1bb41fe5dd4b53fe433a13
Fan Sun <61806088@qq.com>
2025-06-10 21:17:37 +0800

    Initial Commit

---
+++ LPhoto/LPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LPhotoApp.swift
+//  LPhoto
+//
+//  Created by 孙凡 on 2025/6/10.
+//
+
+import SwiftUI
+
+@main
+struct LPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LPhotoTests/LPhotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LPhotoTests.swift
+//  LPhotoTests
+//
+//  Created by 孙凡 on 2025/6/10.
+//
+
+import Testing
+@testable import LPhoto
+
+struct LPhotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
